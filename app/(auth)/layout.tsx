81d15b8fcfb0c35baed6bcf18d5bd28078225c0e
sajika <sjdkhedmati1@gmail.com>
2024-01-14 00:46:53 +0330

    sign-in + sign-up page template

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<main className="w-screen h-screen flex justify-center items-center">
-			{children}
+			<section className="flex flex-col items-center">
+				<div className="flex flex-col items-center gap-2 mb-4">
+					<div className="w-32 h-32 flex items-center justify-center">
+						<Image src="/logo.png" width={120} height={120} alt="linker logo" />
+					</div>
+					<div className="text-center">
+						<h1 className="text-2xl font-semibold">LINKER</h1>
+						<p className="text-sm text-gray-700">
+							Introduce yourself to others in the most professional way
+						</p>
+					</div>
+				</div>
+				{children}
+			</section>
 		</main>
 	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+	const user = await currentUser();
+
 	return (
 		<main className="w-screen h-screen">
 			<p>liker application</p>
+			{!user && (
+				<SignInButton afterSignInUrl="/">
+					<Button>Sign In</Button>
+				</SignInButton>
+			)}
 			<UserButton afterSignOutUrl="/" />
 		</main>
 	);
